b00cbe810a8665a4d17924b0dfc1f9537e53c3fd
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-10-21 21:47:41 +0545

    promises assignment task 1 :white_check_mark

---
+++ wlit/assignment/Promises&callbacks/first_task/script.js
@@ -0,0 +1,16 @@
+const DEMO_URL ="https://jsonplaceholder.typicode.com/posts/1"
+const request = fetch(DEMO_URL)
+    .then((response) =>{
+    console.log(response);
+    return response.json();
+})
+.then((data) => {
+    document.getElementById("title").textContent="Title : "+data.title
+    document.getElementById("Id").textContent="ID : "+data.id
+    document.getElementById("UserId").textContent="UserId : "+data.userId
+    document.getElementById("Body").textContent="Body : "+data.body
+
+})
+.catch ((error)=>{
+    console.log("error: ",error)
+})
